a570bdaec40c6244c899bff3fe901071c37f071b
hanjae <hanjae.jea@gmail.com>
2019-07-15 14:52:49 +0900

    login 설정 완료

--- TimeLine/TimeLine.py
+++ TimeLine/TimeLine.py
@@ -1,9 +1,30 @@
 import os
 import sqlite3
-from flask import Flask, request, session, render_template, flash
+from datetime import timedelta
+from flask import Flask, request, session, render_template, flash, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'eQv3$2neLj'
+app.permanent_session_lifetime = timedelta(hours=2)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form['password'] == 'timeline' and request.form['userid'] == 'timeline':
+            session['userid'] = request.form['userid']
+        return redirect(url_for('index'))
+        
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    if 'userid' in session:
+        del session['userid']
+    return redirect(url_for('index'))
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'userid' in session:
+        username = session['userid']
+        return render_template('index.html', name=username)
+    return render_template('landing.html')
